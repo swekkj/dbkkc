6512ac44d4883144652eb3764191f3263ea9508e
dinohee <kkrs0512@naver.com>
2018-12-10 21:27:04 +0900

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit:5,
+  host : 'localhost',
+  user : 'root',
+  database: 'db',
+  password: 'passwd'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+      res.render('index');
+      connection.release();
+
+});
+
+module.exports = router;
