5c8b42c92176d0ac8424c18cf6f0172de965dcf5
CJE <jaeen1113@naver.com>
2018-12-12 21:22:02 +0900

    20181212

--- routes/index.js
+++ routes/index.js
@@ -27,12 +27,11 @@ var pool = mysql.createPool({
 router.get('/', function(req, res, next) {
       res.render('before_login');
       connection.release();
-
 });
 
 
 // singup
-router.post('/', upload.single('user_image'), function(req, res, next) {
+router.post('/', upload.single('user_img'), function(req, res, next) {
 
   console.log('hello');
   var user_id = req.body.user_id;
@@ -48,6 +47,7 @@ router.post('/', upload.single('user_image'), function(req, res, next) {
   var member_data = [user_id, user_pw, user_nickname, user_image, user_phone];
   var personality_data = [user_name, user_birth, user_address, user_phone, user_email];
 
+
   pool.getConnection(function (err, connection) {
     // personality table
     console.log(personality_data);
@@ -62,7 +62,7 @@ router.post('/', upload.single('user_image'), function(req, res, next) {
     connection.query(sqlforMEMBER, member_data, function(err, rows){
       console.log('in member query');
       if(err) console.error(err);
-      res.redirect('before_login');
+      res.redirect('/');
       connection.release();
     });
 
@@ -71,14 +71,12 @@ router.post('/', upload.single('user_image'), function(req, res, next) {
 });
 
 
+
 //signin
 router.post('/signin', function(req, res, next){
 
   var id = req.body.conn_id;
   var pw = req.body.conn_pw;
-
-  console.log([id,pw]);
-
 
   pool.getConnection(function (err, conn) {
     if(err) console.error(err);
@@ -92,8 +90,12 @@ router.post('/signin', function(req, res, next){
       {res.redirect("/");}
       else
       {
-        res.render('index', {rows: result} );
-        conn.release();
+        var newsql = "select * from personality where phone=?";
+        var pho = result[0].phone;
+        conn.query(newsql, [pho], function(err, other){
+          res.render('index', {member: result, personality:other} );
+          conn.release();
+        });
       }
     });
   });
